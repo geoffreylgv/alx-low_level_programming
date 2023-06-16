6a618770a8ce0ca5c7a56e68b15100874d5ab07e
geoffreylgv <geoffreylogovi2@gmail.com>
2023-06-16 06:18:32 -0700

    4 print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - The code main block
+ * Return - return 0 (runs with success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

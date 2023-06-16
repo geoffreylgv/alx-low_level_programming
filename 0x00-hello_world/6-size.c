ded05f5d8f54c813f1aa2a1b9967cf6cfde2e08a
geoffreylgv <geoffreylogovi2@gmail.com>
2023-06-16 06:45:50 -0700

    print type size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main block that does specific tasks(especialy shows a size of different type of computer using c programming language
+ * Return: 0 (run w/ success)
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}

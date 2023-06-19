33ec067c27d9cb77b36af0154e2afc945839f649
geoffreylgv <geoffreylogovi2@gmail.com>
2023-06-19 08:09:52 -0700

    fixe 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,10 @@ int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-	putchar(c);
-	if (c != '9')
-		{
+		putchar(c);
+		if (c != '9')
 			putchar(',');
 			putchar(' ');
-		}
 	}
 	putchar('\n');
 	return (0);

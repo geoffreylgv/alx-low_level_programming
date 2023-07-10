0db207e525898619231f0b44f3e7e45ee0150640
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-10 08:53:44 -0700

    fixe space prohibited line 11 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int i;
 	char letter;
 
-	for ( i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);

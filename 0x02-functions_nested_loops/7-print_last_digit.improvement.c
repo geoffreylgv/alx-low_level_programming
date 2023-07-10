af41275301be96a79fb8cc3fbcd5408a2d4a86c2
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-10 10:17:43 -0700

    rever the improvement

--- 0x02-functions_nested_loops/7-print_last_digit.improvement.c
+++ 0x02-functions_nested_loops/7-print_last_digit.improvement.c
@@ -8,12 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-
-	if (last_digit < 0)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
+	int last_digit = abs(n) % 10;
 
 	return (last_digit);
 }

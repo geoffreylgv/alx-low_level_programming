0bb91038ee3434eca6f60271f7f49d3295434533
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-12 09:12:30 -0700

    fixe 6-print_line.c clean code

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,15 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	} else
-	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
 	}
+
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
 
 }
 

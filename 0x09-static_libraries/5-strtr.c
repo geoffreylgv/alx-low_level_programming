89d8525a11c2f900a1d8c8526d919167824c9fda
geoffreylgv <evanomarkez@gmail.com>
2023-07-14 07:34:23 +0000

    add files to make librarie

---
+++ 0x09-static_libraries/5-strtr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - _strstr function
+ * @haystack: input
+ * @needle: input
+ * Return: integer success
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	return (0);
+}

64e3fefcaecb2f344036a4941edc42f683b61c59
geoffreylgv <evanomarkez@gmail.com>
2023-07-17 02:22:23 +0000

    print the number of arg passed to function

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the number of arguments passed to it
+* @argc: arg count
+* @argv: array vector
+* Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
+

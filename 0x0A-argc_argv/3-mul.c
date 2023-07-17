7adb5b4f858f2bad6914b581c2e29e6c87f73095
geoffreylgv <evanomarkez@gmail.com>
2023-07-17 02:33:54 +0000

    multiplacates two numbers and if error returns error with code 1 otherwise does the work

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+/**
+ * main - multiply two numbers
+ * @argc: arg count
+ * @argv: array of arg count
+ * Return: result of multiplication or 1
+ **/
+int main(int argc, char *argv[])
+{
+	int a, b, response;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/* atoi convert a string to an int*/
+	/* i can use my own atoi written before as well*/
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	response = a * b;
+	printf("%d\n", response);
+	return (0);
+}
+

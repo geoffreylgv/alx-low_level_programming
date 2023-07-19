8e2c538449f6e6eff10f872aeb57479365ad5697
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-19 14:52:16 -0700

    function pointers what is your name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - Function that prints a name.
+ * @name: Name to print
+ * @f: Pointer to void function, param char* arg
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+# 0x0F-function_pointers
+> go go go

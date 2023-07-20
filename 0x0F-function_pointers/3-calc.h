3bba6d56422809a9a39da456661eca5afc4f4dd4
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-20 11:22:46 -0700

    push A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,23 @@
+#ifndef CALC_H
+#define CALC_H
+
+/**
+ * struct op - Struct op
+ * @op: operator
+ * @f: operator function
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int, int);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
+

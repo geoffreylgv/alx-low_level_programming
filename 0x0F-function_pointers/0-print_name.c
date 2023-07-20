56551fd81d6f85187a74fd5ef90423ef3c0c9976
geoffreylgv <geoffreylogovi2@gmail.com>
2023-07-20 02:19:41 -0700

    fixe 0-print_name.c name or ptr must not be null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
@@ -7,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL) return;
 	f(name);
 }

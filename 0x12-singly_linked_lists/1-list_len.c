4f4c2b9268953e48a82b27916739e80204fa0e2c
geoffreylgv <geoffreylogovi2@gmail.com>
2023-08-01 03:53:40 -0700

    3-add_node_end.c fixed

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,7 +6,7 @@
  *
  * Return: count
  */
-size_t list_len(const size_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count;
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <strings.h>
 
 /**
  * add_node_and - add node to the end of linkedlist
@@ -9,25 +10,43 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *nouvelle;
+	list_t *tete = *head;
 
-	new = malloc(sizeof(list_t));
-	if (new != NULL)
+	nouvelle = malloc(sizeof(list_t));
+	if (nouvelle != NULL)
 	{
-		new->str = strdup(str);
-		new->len = _strlen(str);
-		new->next = NULL;
+		nouvelle->str = strdup(str);
+		nouvelle->len = _strlen(str);
+		nouvelle->next = NULL;
 	}
 	else
 		return (NULL);
-	if (head != NULL)
+	if (tete != NULL)
 	{
-		while (head->next != NULL)
-			head = head->next;
-		head->next = new;
+		while (tete->next != NULL)
+			tete = tete->next;
+		tete->next = nouvelle;
 	}
 	else
-		*head = new;
-	return (new);
+		*head = nouvelle;
+	return (nouvelle);
 }
+
+
+/**
+ * _strlen - counts the length of a string
+ * @str: string to be counted
+ *
+ * Return: the length of str
+ */
+int _strlen(const char *str)
+{
+        int i = 0;
+
+        while (str[i] != '\0')
+                i++;
+        return (i);
+}
+
 

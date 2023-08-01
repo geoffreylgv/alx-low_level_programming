729a912adedbe26e476e512b870d4c9f3cfbf610
geoffreylgv <geoffreylogovi2@gmail.com>
2023-08-01 04:18:37 -0700

    fixes checker 10/11 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,43 +10,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *nouvelle;
-	list_t *tete = *head;
+	list_t  *new, *ptr;
+	unsigned int len = 0;
 
-	nouvelle = malloc(sizeof(list_t));
-	if (nouvelle != NULL)
+	new = malloc(sizeof(list_t));
+	if (new)
 	{
-		nouvelle->str = strdup(str);
-		nouvelle->len = _strlen(str);
-		nouvelle->next = NULL;
-	}
-	else
+		while (*(str + len))
+			len++;
+		new->str = strdup(str);
+		if (new->str)
+		{
+			new->len = len;
+			new->next = NULL;
+			if (!*head)
+			{
+				*head = new;
+				return (new);
+			}
+			ptr = *head;
+			while (ptr->next)
+				ptr = ptr->next;
+			ptr->next = new;
+			return (new);
+		}
+		free(new);
 		return (NULL);
-	if (tete != NULL)
-	{
-		while (tete->next != NULL)
-			tete = tete->next;
-		tete->next = nouvelle;
 	}
-	else
-		*head = nouvelle;
-	return (nouvelle);
-}
-
-
-/**
- * _strlen - counts the length of a string
- * @str: string to be counted
- *
- * Return: the length of str
- */
-int _strlen(const char *str)
-{
-        int i = 0;
-
-        while (str[i] != '\0')
-                i++;
-        return (i);
+	return (NULL);
 }
 
 

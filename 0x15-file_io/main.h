71deae9a9e3cb40f9e0cc98aee513bc8871752b6
geoffreylgv <geoffreylogovi2@gmail.com>
2023-08-08 06:31:47 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
